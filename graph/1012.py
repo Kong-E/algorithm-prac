4d31218fcdd08476593d64ee41b90344c242c55f
Kong-E <kng001016@dgu.ac.kr>
2024-01-03 09:49:25 +0900

    1012: visited를 대체한 버전

--- graph/1012.py
+++ graph/1012.py
@@ -3,9 +3,9 @@ from collections import deque
 x_arr = [0, 0, -1, 1]
 y_arr = [1, -1, 0, 0]
 
-def bfs(graph, start_x, start_y, visited):
+def bfs(graph, start_x, start_y):
     queue = deque([(start_x,start_y)])
-    visited[start_x][start_y] = True
+    graph[start_x][start_y] = 0
     
     while queue:
         (x, y) = queue.popleft();
@@ -13,9 +13,9 @@ def bfs(graph, start_x, start_y, visited):
         for idx in range(4):
             nx = x + x_arr[idx]
             ny = y + y_arr[idx]
-            if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and graph[nx][ny] == 1 and not visited[nx][ny]:
+            if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and graph[nx][ny] == 1:
                 queue.append((nx,ny))
-                visited[nx][ny] = True
+                graph[nx][ny] = 0
 
 T = int(input())
 result = []
@@ -23,7 +23,6 @@ result = []
 for _ in range(T):
     M, N, K = map(int, input().split())
     graph = [[0 for _ in range(N)] for _ in range(M)]
-    visited = [[False for _ in range(N)] for _ in range(M)]
 
     for _ in range(K):
         x, y = map(int, input().split())
@@ -32,8 +31,8 @@ for _ in range(T):
     count = 0
     for x in range(M):
         for y in range(N):
-            if graph[x][y] == 1 and not visited[x][y]:
-                bfs(graph, x, y, visited)
+            if graph[x][y] == 1:
+                bfs(graph, x, y)
                 count += 1
 
     result.append(count)
