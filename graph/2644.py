fcd26b6cc4417736db23994a80b7bbd09e053f96
Kong-E <kng001016@dgu.ac.kr>
2024-01-03 17:58:57 +0900

    2644: 촌수 카운트 하는 법

--- graph/2644.py
+++ graph/2644.py
@@ -6,6 +6,7 @@ m = int(input()) # 부모 자식들 간의 관계의 개수
 
 graph = {i+1:[] for i in range(n)}
 visited = [0] * (n+1)
+
 for _ in range(m):
     x, y = map(int, input().split()) # x는 y의 부모
     graph[x].append(y)
@@ -13,21 +14,19 @@ for _ in range(m):
     
 def bfs(start_node, target_node):
     queue = deque([start_node])
-    visited[start_node] = 1
+    visited[start_node] = 0  # 시작 노드의 촌수는 0
     
     while queue:
         node = queue.popleft()
         
+        if node == target_node:
+            return visited[node]  # 타겟 노드에 도달했을 때 촌수 반환
+
         for adj_node in graph[node]:
             if visited[adj_node] == 0:
                 queue.append(adj_node)
-                visited[adj_node] = 1 
-    return -1
+                visited[adj_node] = visited[node] + 1  # 촌수 업데이트
+
+    return -1  # 타겟 노드에 도달하지 못함
                     
-# bfs(a,b)
-
-for node in graph:
-    print(f"{node} : {graph[node]}")
-
-# 촌수 카운트를 어떻게 하지?
-    
+print(bfs(a, b))
