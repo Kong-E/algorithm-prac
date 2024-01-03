b78f77fd47853b42d4fbbc0055788675fec15dd8
Kong-E <kng001016@dgu.ac.kr>
2024-01-03 14:40:57 +0900

    2606: 더 나은 visited 처리

--- graph/2606.py
+++ graph/2606.py
@@ -5,8 +5,7 @@ sys.setrecursionlimit(10000)
 n = int(input())  # Assuming n is a single integer
 k = int(input())  # Assuming k is a single integer
 graph = {i+1: [] for i in range(n)}
-visited = [False] * (len(graph)+1)
-count = 0
+visited = [0] * (n+1)
 
 for _ in range(k):
     x, y = map(int, input().split())
@@ -14,14 +13,11 @@ for _ in range(k):
     graph[y].append(x)
     
 def dfs(start_node):
-    global count
-    visited[start_node] = True
+    visited[start_node] = 1
     
     for adj_node in graph[start_node]:
-        if not visited[adj_node]:
-            count += 1
+        if visited[adj_node] == 0:
             dfs(adj_node)
-            
-    return count
     
-print(dfs(1))
+dfs(1)
+print(sum(visited)-1)
