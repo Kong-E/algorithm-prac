2558951aa55dbf24a2299e9b62886af71c516c7b
Kong-E <kng001016@dgu.ac.kr>
2024-01-04 10:43:26 +0900

    2644: dfs로 풀기

--- graph/2644.py
+++ graph/2644.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 n = int(input()) # 전체 사람 수
 a, b = map(int, input().split()) # 계산해야 하는 두 사람의 번호
 m = int(input()) # 부모 자식들 간의 관계의 개수
@@ -12,21 +10,22 @@ for _ in range(m):
     graph[x].append(y)
     graph[y].append(x)
     
-def bfs(start_node, target_node):
-    queue = deque([start_node])
-    visited[start_node] = 0  # 시작 노드의 촌수는 0
+def dfs(start_node, target_node):
+    stack = [(start_node, 0)]
     
-    while queue:
-        node = queue.popleft()
-        
-        if node == target_node:
-            return visited[node]  # 타겟 노드에 도달했을 때 촌수 반환
+    while stack:
+        (node, count) = stack.pop() 
+              
+        if visited[node] == 0: 
+            visited[node] = 1
+                       
+            if node == target_node:
+                return count  # 타겟 노드에 도달했을 때 촌수 반환
 
-        for adj_node in graph[node]:
-            if visited[adj_node] == 0:
-                queue.append(adj_node)
-                visited[adj_node] = visited[node] + 1  # 촌수 업데이트
+            for adj_node in graph[node]:
+                if visited[adj_node] == 0:
+                    stack.append((adj_node, count+1))
 
     return -1  # 타겟 노드에 도달하지 못함
                     
-print(bfs(a, b))
+print(dfs(a, b))
