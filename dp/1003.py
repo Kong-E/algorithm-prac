50ef9012c4790ffa93f11b230eef401c7c0271a8
Kong-E <kng001016@dgu.ac.kr>
2024-01-04 17:27:35 +0900

    1003: 시간초과

--- dp/1003.py
+++ dp/1003.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.setrecursionlimit(10000)
+
 t = int(input()) # 테스트 케이스 개수
 n_list = []
 count = [0, 0]
@@ -6,20 +10,16 @@ for _ in range(t):
     i = int(input())
     n_list.append(i)
     
-def fibonacci(n, d):
+def fibonacci(n):
     if n == 0:
         count[0] += 1
         return 0
     if n == 1:
         count[1] += 1
         return 1
-    if d[n] != 0:
-        return d[n]
-    d[n] = fibonacci(n-1, d) + fibonacci(n-2, d)
-    return d[n]
+    return fibonacci(n-1) + fibonacci(n-2)
     
 for n in n_list:
-    d = [0] * (n+1)
-    fibonacci(n, d)
+    fibonacci(n)
     print(count[0], count[1])
     count = [0, 0]
