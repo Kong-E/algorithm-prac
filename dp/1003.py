0d6c1f074816320164ed5609a9956b8a8fded40f
Kong-E <kng001016@dgu.ac.kr>
2024-01-04 18:16:39 +0900

    1003: 풀이 사고 과정
    - 피보나치 함수에서 n이 0일 때와 1일 때 count를 1씩 올려주는 방식으로 접근하고자 하였음.
    - dp 문제이기 때문에 별 생각 없이 처음엔 메모이제이션이 있는 피보나치 함수로 접근하였으나, 메모이제이션 된 n의 경우 재귀 호출을 더 이상 하지 않기 때문에 n이 0과 1일 때로 다시 돌아오지 않았음. 그래서 n이 어떤 수이든지 0은 1번, 1은 2번 호출되었음. dp에 대해 이해할 수 있는 좋은 문제라고 생각함.
    - 메모이제이션을 빼면 0과 1의 호출횟수가 정상적으로 세어졌음. 그러나 제출 시 시간 초과 발생.
    - 0과 1이 호출되는 횟수는 n-1에서의 호출 횟수와 n-2에서의 호출 횟수의 합으로 귀납적으로 결정됨을 깨닫고 피보나치 함수 코드를 삭제함.

--- dp/1003.py
+++ dp/1003.py
@@ -1,25 +1,19 @@
-import sys
-
-sys.setrecursionlimit(10000)
-
 t = int(input()) # 테스트 케이스 개수
 n_list = []
-count = [0, 0]
 
 for _ in range(t):
     i = int(input())
     n_list.append(i)
-    
-def fibonacci(n):
-    if n == 0:
-        count[0] += 1
-        return 0
-    if n == 1:
-        count[1] += 1
-        return 1
-    return fibonacci(n-1) + fibonacci(n-2)
-    
+
 for n in n_list:
-    fibonacci(n)
-    print(count[0], count[1])
-    count = [0, 0]
+    s = [0,0] * (n+1) # 0과 1의 개수를 메모이제이션
+    
+    for i in range(0,n+1):
+        if i == 0:
+            s[i] = [1, 0]
+        elif i == 1:
+            s[i] = [0, 1]
+        else:
+            s[i] = [s[i-1][0]+s[i-2][0], s[i-1][1]+s[i-2][1]]
+            
+    print(s[n][0], s[n][1]) 
