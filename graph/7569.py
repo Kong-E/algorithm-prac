0b7ca2cdd01a73fa1455e5706ca009e4d7133b8f
Kong-E <kng001016@dgu.ac.kr>
2024-01-08 11:15:10 +0900

    7569: 기존 코드의 문제점
    1. dy가 1이거나 -1일 때 같은 층이어야 함을 고려하지 않았음
    2. 인접한 지점(nx,ny)에 기존의 지점(x,y) + 1을 할당하는 방법은 최소 일수를 구하는 데 적합한 방법이 아님 (동시성을 고려하지 않기 때문에)
    3. day 변수 도입
    4. 인접한 지점(nx,ny)에 day + 1 값을 직접할당하는 것도 좋지 않음 (기존에 익어 있던 토마토에서만 bfs를 실행하고 싶기 때문에)
    5. 초기 익은 토마토에서만 bfs를 실행하는 것이 필요함

--- graph/7569.py
+++ graph/7569.py
@@ -13,22 +13,26 @@ for i in range(H):
       graph[j+(i*N)] = a
 
 def bfs(start_x,start_y): # x = m (열), y = n (행)
-  queue = deque([(start_x,start_y)])
+  queue = deque([(start_x, start_y, 0)])  # 좌표와 날짜를 함께 큐에 저장
 
   while queue:
-    (x, y) = queue.popleft()
+    x, y, day = queue.popleft()
 
     for i in range(6): # 얘네는 하루 걸림 -> 다 돌고 난 후에 count += 1
       nx = x + dx[i]
-      ny = y + dy[i]
+      ny = y + dy[i] # dy가 1이거나 -1일 때 같은 층이어야 함을 고려해야함 !!!!
 
+      # 범위를 벗어나면 건너뜀
       if nx < 0 or nx >= M or ny < 0 or ny >= N*H:
-          continue
-      if graph[ny][nx] == -1:
-          continue
-      if graph[ny][nx] == 0:
-        queue.append((nx,ny))
-        graph[ny][nx] = graph[y][x] + 1
+        continue
+      # 같은 층이 아니면 건너뜀
+      if (ny == y + 1 or ny == y - 1) and ny // N != y // N:
+        continue
+      # 토마토가 들어있지 않은 칸이면 건너 뜀
+      if graph[ny][nx] == -1: 
+        continue
+      queue.append((nx, ny, day + 1))
+      graph[ny][nx] = day + 1  # 날짜로 갱신
 
 for i in range(N*H):
    for j in range(M):
@@ -45,7 +49,13 @@ for row in graph:
         if element > max_value:
             max_value = element
 
+# # 그래프 출력
+# for node in graph:
+#     print(node)
+
 if unripe_exist:
   print(-1)
+elif max_value == 1:
+  print(0)
 else:
-  print(max_value-1)
+  print(max_value)
