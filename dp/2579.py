1c13f8d69ca5e8fe739ad8cfe56ac2e41b015169
Kong-E <kng001016@dgu.ac.kr>
2024-01-09 09:28:00 +0900

    2579: 최종 코드
    1. 런타임 에러 발생 원인: n이 1일 때 stairs[2]나 stairs[3] 등에 접근할 수 없어서
    2. dp[1]과 dp[2]는 각각의 경우의 최댓값만 생각하면 됨 (건너뜀 여부는 생각하지 않아도 됨)

--- dp/2579.py
+++ dp/2579.py
@@ -1,14 +1,22 @@
-n = int(input()) # 계단의 개수
-stairs = [0] * (n+1)
+import sys
 
-for i in range(1, n+1):
-    stairs[i]=(int(input()))
+# input 함수를 재정의하지 않고, 사용할 때마다 rstrip()을 적용합니다.
+def input():
+    return sys.stdin.readline().rstrip()
+
+n = int(input())  # 계단의 개수
+stairs = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    stairs[i] = int(input())
     
 # dp 배열을 초기화합니다.
 dp = [0] * 301
 dp[1] = stairs[1]
-dp[2] = stairs[1] + stairs[2]
-dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+if n >= 2:
+    dp[2] = stairs[1] + stairs[2]
+if n >= 3:
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
 
 # 점화식을 계산합니다.
 for i in range(4, n + 1):
