eb17ac0297b3fd6021f217eea7b1ee820a2830ac
Kong-E <kng001016@dgu.ac.kr>
2024-01-09 15:21:24 +0900

    2747: 복기
    - 입력받는 n일 때 dp 테이블 초기화 시 주의 (n이 1이면 dp[2]는 존재하지 않음)
    - n번째 수를 구하라 = 인덱스 상으로는 n+1

--- dp/2747.py
+++ dp/2747.py
@@ -6,10 +6,13 @@ n = int(input())
 d = [0] * (n+1) # dp 테이블
 
 d[1] = 1
-d[2] = 1
 
-for i in range(3, n+1):
-    d[i] = d[i-1] + d[i-2]
+if n >= 2:
+    d[2] = 1
+
+if n >= 3:
+    for i in range(3, n+1):
+        d[i] = d[i-1] + d[i-2]
     
 print(d[n])
 
