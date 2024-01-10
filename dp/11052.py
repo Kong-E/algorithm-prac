c96367c264eec61fed81eb03214c8be0ddf75ccb
Kong-E <kng001016@dgu.ac.kr>
2024-01-10 15:18:01 +0900

    11052: 복기
    첫 번째 for문(for i in range(1, n+1))은 각 카드 개수에 대해 반복합니다.
    두 번째 for문(for j in range(1, i+1))은 주어진 i개의 카드를 구매하는 모든 가능한 방법을 고려합니다.

--- dp/11052.py
+++ dp/11052.py
@@ -3,13 +3,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-prices = [0] + list(map(int, input().split()))
+p = [0] + list(map(int, input().split()))
 
 dp = [0] * (n+1)
 
-dp[1] = prices[1]
-
-for i in range(2, n+1):
-    dp[i] = max(dp[i-1]+prices[1], prices[i])
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j]+p[j])
     
 print(dp[n])
