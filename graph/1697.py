7dd0a8540d68a1fe5be02ae7e53d8d9b9ea0ee71
Kong-E <kng001016@dgu.ac.kr>
2024-01-18 12:25:34 +0900

    1697: 메모리 및 시간 단축

--- graph/1697.py
+++ graph/1697.py
@@ -9,17 +9,17 @@ visited = [False] * 100001
 
 while queue:
   node, time = queue.pop(0)
-  
-  if not visited[node]:
-    visited[node] = True
       
-    if node - 1 >= 0:
-      queue.append((node-1, time+1))
-    if node + 1 <= 100000:
-      queue.append((node+1, time+1))
-    if node * 2 <= 100000:
-      queue.append((node*2, time+1))
-      
-    if node == k:
-      print(time)
-      break
+  if node - 1 >= 0 and not visited[node-1]:
+    queue.append((node-1, time+1))
+    visited[node-1] = True
+  if node + 1 <= 100000 and not visited[node+1]:
+    queue.append((node+1, time+1))
+    visited[node+1] = True
+  if node * 2 <= 100000 and not visited[node*2]:
+    queue.append((node*2, time+1))
+    visited[node*2] = True
+    
+  if node == k:
+    print(time)
+    break
