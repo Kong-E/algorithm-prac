52ed5514cae71b165bf6803b35372ee0abc15e75
Kong-E <kng001016@dgu.ac.kr>
2024-01-22 12:51:59 +0900

    1550: ord를 이용해 구현
    ord() 함수는 파이썬에서 문자(character)를 그 문자의 ASCII 코드나 유니코드 값(정수)으로 변환하는 데 사용돼. 간단히 말해서, ord() 함수는 주어진 문자에 해당하는 숫자를 반환하는 거야.

--- math/1550.py
+++ math/1550.py
@@ -1,5 +1,22 @@
 hex_number = input()
 
-decimal_number = int(hex_number, 16)
+# 16진수를 10진수로 변환하기
+decimal_number = 0
+length = len(hex_number)
+
+for i in range(length):
+    # 현재 자릿수의 문자를 가져옴
+    digit = hex_number[i]
+
+    # 16진수의 각 자릿수를 10진수로 변환
+    if '0' <= digit <= '9':
+        value = ord(digit) - ord('0')
+    elif 'A' <= digit <= 'F':
+        value = ord(digit) - ord('A') + 10
+    elif 'a' <= digit <= 'f':
+        value = ord(digit) - ord('a') + 10
+
+    # 변환된 값을 적절한 자릿수의 16의 거듭제곱과 곱하여 더함
+    decimal_number += value * (16 ** (length - 1 - i))
 
 print(decimal_number)
