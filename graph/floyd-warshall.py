71bbdd5e057241d39438b6a0a454b94ecc0d2570
Kong-E <kng001016@dgu.ac.kr>
2024-01-22 13:15:47 +0900

    플로이드 워셜 소스코드 수정

--- graph/floyd-warshall.py
+++ graph/floyd-warshall.py
@@ -14,8 +14,8 @@ for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a][b] = c
     
-for k in range(1, n + 1):
-    for a in range(1, n + 1):
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
         if graph[a][b] == INF:
             print("INFINITY", end=" ")
         else:
