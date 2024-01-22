405b4edad916286550fc0e8cc9fe04af05bf3c8a
Kong-E <kng001016@dgu.ac.kr>
2024-01-22 14:38:21 +0900

    15649: 백트래킹 직접 구현
    n = 4, m = 3
    스택의 변화             | 실행되는 동작           | 비고
    []                      | 시작                    |
    [backtracking([1])]     | 1을 첫 번째 원소로 선택 | 1 선택
    [backtracking([1, 2])]  | 2를 두 번째 원소로 선택 | 2 선택
    [backtracking([1, 2, 3])] | 3을 세 번째 원소로 선택 | 순열 [1, 2, 3] 완성, 출력
    [backtracking([1, 2])]  | backtracking([1, 2, 3]) 종료 | 3 반환
    [backtracking([1])]     | backtracking([1, 2]) 종료 | 2 반환
    [backtracking([1, 3])]  | 3를 두 번째 원소로 선택 | 3 선택
    [backtracking([1, 3, 2])] | 2를 세 번째 원소로 선택 | 순열 [1, 3, 2] 완성, 출력
    [backtracking([1, 3])]  | backtracking([1, 3, 2]) 종료 | 2 반환
    [backtracking([1])]     | backtracking([1, 3]) 종료 | 3 반환
    [backtracking([])]      | backtracking([1]) 종료 | 1 반환
    ...                      | backtracking([])를 시작점으로 새로운 선택 시작 | 1 반환 후 2 선택 시작

--- backtracking/15649.py
+++ backtracking/15649.py
@@ -1,15 +1,18 @@
 import sys
-import itertools
 
 input = sys.stdin.readline
 
+def generate_permutations(sequence, chosen, m):
+    if len(chosen) == m:  # 원하는 길이의 순열이 완성됐으면
+        print(' '.join(map(str, chosen)))  # 순열을 출력하고
+        return  # 이 경로는 끝났으니 돌아가기
+      
+    for i in sequence:
+        if i in chosen:
+            continue
+        chosen.append(i)
+        generate_permutations(sequence, chosen, m)
+        chosen.pop() # 다른 숫자를 시도하기 위해 마지막 숫자 제거
+
 n, m = map(int, input().split())
-
-items = list(range(1,n+1))
-
-all_permutations = list(itertools.permutations(items, m))
-
-for perm in all_permutations:
-  for p in perm:
-    print(p, end=' ')
-  print()
+generate_permutations(range(1, n+1), [], m)
