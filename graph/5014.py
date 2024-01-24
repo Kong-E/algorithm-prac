489dbd8531232aeba0c5f6ab65288fc3c84495ef
Kong-E <kng001016@dgu.ac.kr>
2024-01-24 20:37:21 +0900

    5014: 방법2

--- graph/5014.py
+++ graph/5014.py
@@ -6,29 +6,22 @@ input = sys.stdin.readline
 F, S, G, U, D = map(int, input().split())
 result = 0
 
-def bfs(start, end, visited):
-    global result
-    queue = deque([start])
+def bfs(start, end):
+    visited = [False] * (F + 1)
+    queue = deque([(start, 0)])  # (현재 층, 이동 횟수)
     visited[start] = True
 
     while queue:
-        result += 1
-        for _ in range(len(queue)):
-            v = queue.popleft()
-            if v == end:
-                return
-            if v + U <= F and not visited[v + U]:
-                queue.append(v + U)
-                visited[v + U] = True
-            if v - D > 0 and not visited[v - D]:
-                queue.append(v - D)
-                visited[v - D] = True
+        v, count = queue.popleft()
+        if v == end:
+            return count
+        if v + U <= F and not visited[v + U]:
+            queue.append((v + U, count + 1))
+            visited[v + U] = True
+        if v - D > 0 and not visited[v - D]:
+            queue.append((v - D, count + 1))
+            visited[v - D] = True
 
-    result = 'use the stairs'
-    
-visited = [False] * (F + 1)
-bfs(S, G, visited)
-if result == 'use the stairs':
-    print(result)
-else:
-    print(result - 1)
+    return 'use the stairs'
+
+print(bfs(S, G))
