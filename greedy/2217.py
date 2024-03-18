2210a4c2b7bf51fc80fce431aa9952ac321ad98e
Kong-E <kng001016@dgu.ac.kr>
2024-03-18 13:57:38 +0900

    2217
    n <= 10000 이므로 모든 경우의 수를 따져도 됨

---
+++ greedy/2217.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+ropes = [int(input()) for _ in range(n)]
+
+ropes.sort(reverse=True)
+
+max_weight = 0
+
+for i in range(n):
+    max_weight = max(max_weight, ropes[i]*(i+1))
+
+print(max_weight)
